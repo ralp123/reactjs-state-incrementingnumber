b640a73e7ef4fd5dd7581991d04eef775fea7cf1
ralp123 <rapie1247p@gmail.com>
2019-08-09 13:40:38 +0800

    Delete Welcome.js

--- src/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-
-function Welcome(props) {
-    return <h3>Hello, {props.name}</h3>;
-}
-
-export default Welcome;
